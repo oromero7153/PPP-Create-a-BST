eb002e57919d3d36f08c15eea00424cc3c7376e1
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-11-15 21:42:55 -0800

    added pseudocode

--- BST.py
+++ BST.py
@@ -2,3 +2,11 @@
 
 #Part 2: Create a BST class
   #Part 3: Add functionality to your BST class
+
+
+#If the tree is empty (root points to None), put the new node at the top of the tree.
+    #If the tree is not empty, start at the root. 
+    #Compare the new node's value to the current node's value. 
+        #If the new node is bigger, move to the right. 
+        #If the new node's value is smaller, move to the left. 
+        #When there is no node at the current position, put the new node there.
